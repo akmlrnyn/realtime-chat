c2ba25246e3d505dd85f8f4596ec0da82aa96293
akmalranyan <akmalranyan@gmail.com>
2023-06-05 11:21:01 +0700

    fixed routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Livewire\Chat\CreateChat;
+use App\Http\Livewire\Chat\Main;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +20,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/users', CreateChat::class)->name('users');
+
+Route::get('/chat{key?}', Main::class)->name('chat');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 require __DIR__.'/auth.php';
