cd78b23615b616b364e09c32e986d0b46cfd9d2a
akmalranyan <akmalranyan@gmail.com>
2023-06-05 11:21:45 +0700

    create new chat

--- app/Http/Livewire/Chat/CreateChat.php
+++ app/Http/Livewire/Chat/CreateChat.php
@@ -2,12 +2,46 @@
 
 namespace App\Http\Livewire\Chat;
 
+use App\Models\Conversation;
+use App\Models\Message;
 use App\Models\User;
 use Livewire\Component;
 
 class CreateChat extends Component
 {
     public $users;
+    public $message = 'Hello';
+
+    public function checkConversation($recieverId)
+    {
+        
+        $checkedConversation = Conversation::where('receiver_id', auth()->user()->id)->where('sender_id', $recieverId)
+        ->orWhere('receiver_id', $recieverId)->where('sender_id', auth()->user()->id)->get();
+
+        if (count($checkedConversation) == 0) {
+            $createConversation = Conversation::create([
+                'receiver_id' => $recieverId,
+                'sender_id' => auth()->user()->id,
+                'last_time_message' => 0
+            ]);
+
+            $createMessage = Message::create([
+                'conversation_id' => $createConversation->id,
+                'sender_id' => auth()->user()->id,
+                'receiver_id' => $recieverId,
+                'body' => $this->message
+            ]);
+
+            $createConversation->last_time_message = $createMessage->created_at;
+            $createConversation->save();
+            dd($createMessage);
+            
+        } else if(count($checkedConversation) >= 1) {
+            dd('conversation exist');
+        }
+        
+    }
+
     public function render()
     {
         $this->users = User::where('id','!=', auth()->user()->id)->get();
